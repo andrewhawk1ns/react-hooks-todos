4fe3c329e5541cd30c0b4a2091151c941d08a649
Andrew Hawkins <andrewh@cm2media.ca>
2020-07-05 14:02:41 -0400

    Learning portfolio

---
+++ src/components/TodoForm.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+import uuidv4 from "uuid/v4";
+import React, { useState, useEffect, useContext } from "react";
+import TodosContext from "../context";
+
+export default function TodoForm() {
+  const [todo, setTodo] = useState("");
+  const {
+    state: { currentTodo = {} },
+    dispatch
+  } = useContext(TodosContext);
+
+  useEffect(() => {
+    if (currentTodo.text) {
+      setTodo(currentTodo.text);
+    } else {
+      setTodo("");
+    }
+  }, [currentTodo]);
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+    if (currentTodo.text) {
+      const response = await axios.patch(
+        `https://hooks-api.andrewhaekins.now.sh/todos/${currentTodo.id}`,
+        {
+          text: todo
+        }
+      );
+      dispatch({ type: "UPDATE_TODO", payload: response.data });
+    } else {
+      const response = await axios.post(
+        `https://hooks-api.andrewhaekins.now.sh/todos`,
+        {
+          id: uuidv4(),
+          text: todo,
+          complete: false
+        }
+      );
+      dispatch({ type: "ADD_TODO", payload: response.data });
+    }
+
+    setTodo("");
+  };
+
+  return (
+    <form className="flex justify-center p-5" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="border-black border-solid border-2"
+        onChange={event => setTodo(event.target.value)}
+        value={todo}
+      />
+    </form>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useContext, useReducer, useState, useEffect } from "react";
+import axios from "axios";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+import TodosContext from "./context";
+import todosReducer from "./reducer";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import TodoList from "./components/TodoList";
+import TodoForm from "./components/TodoForm";
+
+const useAPI = (endpoint) => {
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    const response = await axios.get(endpoint);
+    setData(response.data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return data;
+};
+
+const App = () => {
+  const initialState = useContext(TodosContext);
+  const [state, dispatch] = useReducer(todosReducer, initialState);
+  const savedTodos = useAPI("https://hooks-api.andrewhaekins.now.sh/todos");
+
+  useEffect(() => {
+    dispatch({
+      type: "GET_TODOS",
+      payload: savedTodos,
+    });
+  }, [savedTodos]);
+
+  return (
+    <TodosContext.Provider value={{ state, dispatch }}>
+      <TodoForm />
+      <TodoList />
+    </TodosContext.Provider>
+  );
+};
+
+ReactDOM.render(
+  <App />,
+
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
